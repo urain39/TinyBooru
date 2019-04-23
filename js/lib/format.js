57632b52ed965cf88431f625b50c7eaf2912f068
urain39 <urain39@qq.com>
2019-04-24 01:06:56 +0800

    fix: fix cannot format zero index.

--- js/lib/format.js
+++ js/lib/format.js
@@ -2,9 +2,12 @@ if (typeof String.prototype.format !== "function") {
   String.prototype.format = function (obj) {
     var self = this;
     return self.replace(/\{([0-9A-Za-z_$]+)\}/g, function(token) {
-      var key = token.slice(1, token.length - 1);
-      key = Number(key) || key;
-      return obj[key] || "";
+      var key = token.slice(1, token.length - 1),
+          idx = parseInt(key, 10);
+      if (typeof idx === "number") {
+        key = idx;
+      }
+      return obj[key];
     });
   };
 }
